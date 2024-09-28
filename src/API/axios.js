4c828fb5f05b9facc6eb7781d807f8125955a442
LIDYA-GETU <lidyagetu215@gmail.com>
2024-09-27 22:10:58 -0400

    deploying on netlify

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios"
 const axiosInstance= axios.create({
-      baseURL:"http://127.0.0.1:5001/clone-b7179/us-central1/api"
+      // local instanceof firebase functions
+      baseURL:"http://127.0.0.1:5001/clone-b7179/us-central1/api",
+      // deployed version of amazon server on render.com
+      baseURL:"https://amazon-api-deploy-jlpd.onrender.com/"
 });
 export{axiosInstance};
